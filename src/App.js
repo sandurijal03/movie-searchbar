5ac04f2703c27ffab5302f9b5fdb773bff1ff472
Sandip Rijal <sandurijal03@hotmail.com>
2019-05-21 16:16:31 +0545

    adding searchbar and submit button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello There</h1>
+      <h1>Movie Search Engine</h1>
+      <input type="text" placeholder="Enter any movie name" />
+      <input type="button" name="Search" />
     </div>
   );
 }
