b7d63fdd9d0d798fdaa3201b276c91d33fb6095d
Sandip Rijal <sandurijal03@hotmail.com>
2019-05-21 16:23:51 +0545

    Arranging the text position and styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Movie Search Engine</h1>
-      <input type="text" placeholder="Enter any movie name" />
-      <input type="button" name="Search" />
+    <div className="root">
+      <div className="logo">
+        <h1>Movie Search Engine</h1>
+      </div>
+      <div className="search-form">
+        <input type="text" placeholder="Enter any movie name" />
+        <input type="button" value="Search" />
+      </div>
     </div>
   );
 }
