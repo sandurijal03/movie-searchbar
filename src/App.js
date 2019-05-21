054a0ffa6665524f45df86d8c2242341ac39ed3e
Sandip Rijal <sandurijal03@hotmail.com>
2019-05-21 16:36:13 +0545

    changing the functional component into class component and also giving some properties to children

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="root">
-      <div className="logo">
-        <h1>Movie Search Engine</h1>
-      </div>
-      <div className="search-form">
-        <input type="text" placeholder="Enter any movie name" />
-        <input type="button" value="Search" />
-      </div>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.searchMovies = this.searchMovies.bind(this);
+    this.getApi = this.getApi.bind(this);
+  }
 
+  handleChange(e) {
+    this.setState({ movie: e.target.value });
+  }
+
+  searchMovies() {
+    this.getApi();
+  }
+
+  getApi() {}
+
+  render() {
+    return (
+      <div className="root">
+        <div className="logo">
+          <h1>Movie Search Engine</h1>
+        </div>
+        <div className="search-form">
+          <input
+            type="text"
+            placeholder="Enter any movie name"
+            onChange={this.handleChange}
+          />
+          <input type="button" value="Search" onClick={this.searchMovies} />
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
